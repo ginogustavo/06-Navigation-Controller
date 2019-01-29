cbd893558d0bb6c147eee13077af1df7f7c35356
Gustavo Ureta <5447557+ginogustavo@users.noreply.github.com>
2019-01-28 21:50:49 -0500

    Initial Commit

---
+++ 06 Navigation Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  06 Navigation Controller
+//
+//  Created by AcademiamovilesAir on 1/28/19.
+//  Copyright © 2019 index. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
